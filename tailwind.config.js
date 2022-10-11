067688e4865fa32d2ecf28d5c7cd4f7f94f45b57
Tajbid <tajbidrayhan111@gmail.com>
2022-10-11 15:40:30 +0600

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        top:['Permanent Marker']
+      },
+      colors:{
+        "p-c": "var(--primary-color)",
+        "s-c": "var(--secondary-color)",
+        "t-c": "var(--third-color)",
+        "f-c":"var(--fourth-color)",
+      }
+    },
   },
   plugins: [],
 }
